e68f351f8f83e834f16decb02ea24566c9b1480b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-04-20 22:19:39 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  A program using puts function to print
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
