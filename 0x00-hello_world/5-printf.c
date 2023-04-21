131102264f9d5f94bf6097d3f236d8b63b253075
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-04-20 22:45:52 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - A program that uses printf to print
+ * return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
