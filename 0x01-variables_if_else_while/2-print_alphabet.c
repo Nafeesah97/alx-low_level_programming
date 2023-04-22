f57f25e6001185793935867bc61bbdb30106d16a
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-04-22 16:26:46 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints alphabets in lower case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
