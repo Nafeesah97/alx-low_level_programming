f73e47c7b746a68e2fa606f2bbb44df4e53fec56
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-05-04 02:19:51 -0700

    concat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * Returns: a ponter to the resulting string
+ * @dest: string to be concacenated
+ * @src: string to be concacenated
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char* temp = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (*src)
+	{
+		*dest++ = *src++;
+	}	
+	*dest = '\0';
+	return temp;
+}
