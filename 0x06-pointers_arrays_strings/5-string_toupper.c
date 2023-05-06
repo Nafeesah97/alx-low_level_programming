cfe64dc4e7d5bdaf8dd1123405173a2e7732c9e3
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-05-05 17:58:40 -0700

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,12 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	if (str[i] >= 'a' && str[i] <= 'z')
+	while (str[i])
 	{
-		str[i] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
 		i++;
 	}
 	return (str);
