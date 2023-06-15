836be539fe882290277828f2eadfa1e8297daf2d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-06-15 10:53:07 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,24 @@
 #include <stddef.h>
 
 /**
+ * _len - find length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _len(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * str_concat - function that concatenates two strings
  * @s1: sFirst string
  * @s2: Second string
@@ -11,48 +29,34 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2, i;
+	int len1, len2, i, j;
 	char *s;
 
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-	len1 = 0;
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		len1++;
-		i++;
-	}
 
-	len2 = 0;
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		len2++;
-		i++;
-	}
-
+	len1 = _len(s1);
+	len2 = _len(s2);
 	s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
 	i = 0;
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-	while (s2[i] != '\0' )
-	{
-		s1[len1] = s2[i];
-		i++;
-		len1++;
-	}
-	i = 0;
 	while (s1[i] != '\0')
 	{
 		s[i] = s1[i];
 		i++;
 	}
-
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		s[i] = s2[j];
+		i++;
+		j++;
+	}
 	s[i] = '\0';
 	return (s);
 }
