16978a5c97f84d16ecac2d22c1d10d89fa66d3c1
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-06-23 16:09:04 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,16 +15,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	while (c <= 0)
+	c = 0;
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			c = (*cmp)(array[i]);
-		}
-		i++;
+		c = (*cmp)(array[i]);
+		if (c != 0)
+			return (i);
 	}
-	if (c > 0)
-		return (i);
-	else
-		return (-1);
+	return (-1);
 }
