0982eaf32b92f03070b33a6acb33f9bb21aa366e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-07-06 04:26:43 -0700

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,16 +9,11 @@ void free_list(list_t *head)
 {
 	list_t *p;
 
-	p = malloc(sizeof(list_t));
-	if (p == NULL)
-		free(p);
-
 	while (head != NULL)
 	{
 		p = head;
 		head = head->next;
 		free(p->str);
-		free(p->next);
 		free(p);
 	}
 }
