ed407b4757e52963defe88262e0bcc12af2d437a
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-07-06 07:00:57 -0700

    print list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,12 +3,15 @@
 /**
  * listint_len - the number of elements in a linked listint_t list.
  * @h: struct
- * Returns: number of elements
+ * Return: number of elements
  */
 
 size_t listint_len(const listint_t *h)
 {
 	size_t i;
+
+	if (h == NULL)
+		return (0);
 
 	i = 1;
 	while (h->next != NULL)
