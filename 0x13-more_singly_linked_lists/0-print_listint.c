b63c7df025358836f95ca0d5c5ebdd93da04fac0
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-07-06 05:51:00 -0700

    print int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: the integer to be printed
+ * Return: the number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	const listint_t *p;
+
+	if (h == NULL)
+		return (0);
+	p = malloc(sizeof(size_t));
+	if (p == NULL)
+	{
+		return (0);
+	}
+
+	p = h;
+	i = 0;
+	while (p->next != NULL)
+	{
+		printf("%d\n", p->n);
+		p++;
+		i++;
+	}
+	return (i);
+}
