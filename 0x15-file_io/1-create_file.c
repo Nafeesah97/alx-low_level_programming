f078907efc34511c42712f8300d7cf69f1d26b16
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-07-11 03:54:18 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fp <= 0)
+	if (fp == -1)
 		return (-1);
-	i = 0;
-	while (*text_content)
-	{
-		i++;
-		text_content++;
-	}
+
 	if (text_content != NULL)
 	{
+		i = 0;
+        	while (*text_content)
+        	{
+                	i++;
+                	text_content++;
+        	}
 		content = write(fp, text_content, i);
-		if (content <= 0)
+		if (content == -1)
 		{
 			close(fp);
 			return (-1);
