8b97d3da5f804cff161e38f15bb2d86f937badaa
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-07-21 17:14:43 -0700

    s

--- shell_exe/_getenv.c
+++ shell_exe/_getenv.c
@@ -5,19 +5,20 @@ char *_getenv(char* var)
 {
 	char **env = environ;
 	size_t var_len = _strlen(var);
-	char *path;
+	char *path = NULL;
 
-	path = malloc(sizeof(char *) * PATH_MAX);
-	if (path == NULL)
-	{
-		free(path);
-		exit(EXIT_FAILURE);
-	}
 	while (*env != NULL)
 	{
 		if (strncmp(*env, var, var_len) == 0 && (*env)[var_len] == '=') 
 		{
-			path = &(*env)[var_len + 1];
+			path = malloc(_strlen(*env) - var_len);
+			if (path == NULL)
+			{
+				perror("Memory allocation error");
+				exit(EXIT_FAILURE);
+			}
+			strcpy(path, *env + var_len + 1);
+			break;
 		}
 		env++;
 	}
