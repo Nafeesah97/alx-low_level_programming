89fc8a4d71bb172e6f5410ccce172635c77bfe97
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-17 16:08:29 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i, j;
 	dlistint_t *temp;
-	
+
 	if (head == NULL)
 		return (NULL);
 
@@ -37,4 +37,3 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 		return (temp);
 	}
 }
-			
