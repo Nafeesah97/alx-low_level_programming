9547e74728f649b0aa838165271884db66b9529e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-08 15:14:27 -0700

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,6 +18,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	new->size = size;
 	new->array = malloc(sizeof(hash_node_t) * size);
+	if (new->array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		new->array[i] = NULL;
 	return (new);
