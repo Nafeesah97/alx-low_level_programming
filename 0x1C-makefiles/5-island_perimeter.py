f8b7ae2fe725178dc23d663a1665147535136f46
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-10-25 13:28:11 -0700

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    p = 0
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[i]) - 1):
+            if grid[i][j] == 1:
+                if grid[i][j - 1] == 0:
+                    p += 1
+                if grid[i -1][j] == 0:
+                    p += 1
+                if grid[i][j + 1] == 0:
+                    p += 1
+                if grid[i + 1][j] == 0:
+                    p += 1
+    return p
