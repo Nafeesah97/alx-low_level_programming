9d276903189657fcd8eaa4d76aa3b86602ece4b7
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-10-25 14:13:41 -0700

    island update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,14 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
+                if i == len(grid) - 1 or j == len(grid[i]) - 1:
+                    p += 1
                 if grid[i][j - 1] == 0:
                     p += 1
                 if grid[i - 1][j] == 0:
                     p += 1
-                if grid[i][j + 1] == 0:
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 0:
                     p += 1
-                if grid[i + 1][j] == 0:
+                if i < len(grid) - 1 and grid[i + 1][j] == 0:
                     p += 1
     return p
